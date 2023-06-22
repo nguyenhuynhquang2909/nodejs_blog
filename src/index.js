9bd6de95647783144bc986fd22a02bab8298a297
nguyenhuynhquang2909 <92911952+nguyenhuynhquang2909@users.noreply.github.com>
2023-06-22 10:29:04 +0700

    Refractor code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,9 +5,17 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 8080;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json())
+
 
 // Template engine
 app.engine('hbs', handlebars.engine({
@@ -16,15 +24,11 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+route(app);
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}/`)
   console.log(`Example app listening at http://localhost:${port}/news`)
-})
+});
